1402f64dab3e922dc532f2081ae3cdca86efcc3f
fer.rivera95 <fernando_rivera_95@hotmail.com>
2018-10-20 14:52:02 -0300

    a

--- Assets/Scripts/Editor/WindowLoadMaps.cs
+++ Assets/Scripts/Editor/WindowLoadMaps.cs
@@ -1,16 +1,54 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
+using UnityEditor;
 using UnityEngine;
 
-public class WindowLoadMaps : MonoBehaviour {
+public class WindowLoadMaps : EditorWindow
+{
+    [MenuItem("Level options/Load maps")]
+    static void CreateWindow()
+    {
+        var window = ((WindowLoadMaps)GetWindow(typeof(WindowLoadMaps)));
+        window.Show();
+        window.Init();
+    }
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    public void Init()
+    {
+        
+    }
+
+    public void CargarMapas()
+    {
+        List<string> tempPath = new List<string>();
+
+        var asset = AssetDatabase.FindAssets("t:MapsSaved", null);
+
+        for (int i = asset.Length - 1; i >= 0; i--)
+        {
+            //obtengo todo el path
+            string path = AssetDatabase.GUIDToAssetPath(asset[i]);
+            //separo las diferentes carpetas por el carcater /
+            tempPath = path.Split('/').ToList();
+            //obtengo la ultima parte, que seria el nombre con la extension y saco la extension
+            var currentMapName = tempPath.LastOrDefault().Split('.');
+            //si el nombre que obtuve con el que escribi son iguales entonces uso ese scriptable object
+            
+            EditorGUI.BeginDisabledGroup(true);
+            currentMapName[0] = EditorGUILayout.TextField("Map name", currentMapName[0]);            
+            EditorGUI.EndDisabledGroup();
+            if (GUILayout.Button("Delete map"))
+            {
+                AssetDatabase.DeleteAsset(path);
+            }
+        }
+    }
+
+    void OnGUI()
+    {
+        CargarMapas();
+
+    }
+
 }
